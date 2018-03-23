5ce00b6d3110106dbf6266040a865fdf9024142a
Lucy Wall <lhwall@gmail.com>
2018-03-23 23:59:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,15 @@
 require 'pry'
 # Add  code here!
 def prime?(integer)
-    primes_to_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97] 
+    is_prime = true
   primes_array = [2..integer - 1]
   primes_array.each do |i|
-    if i % primes_to_100.each do |j| j == 0
-      primes_array.remove(i)
+    if integer % i == 0
+      return false
+      is_prime = true
   end
 end
+is_prime
 end
 
 #prime?(10)
