3166f617be3e74b09cb6f32c92257200238941f8
Lucy Wall <lhwall@gmail.com>
2018-03-24 00:25:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,15 @@
 require 'pry'
 # Add  code here!
 def prime?(integer)
-  if integer =< 3
-    new_number - integer
-  elsif integer =<
+  if integer != 1 && integer != -1 && integer != 0 && inte
+  number_assign(integer)
 end
 
+def number_assign(integer)
+ if integer >= 3
+    new_number = integer
+  elsif integer =< -3
+    new_number = -1 * integer
+  elsif integer == 1 || integer == -1
+    
 #prime?(10)
