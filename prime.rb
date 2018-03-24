5e2f5952b6d5ea58777c3d97f522fad00d650d5e
Lucy Wall <lhwall@gmail.com>
2018-03-24 00:35:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,14 @@ def prime?(integer)
     new_number = -1 * integer
   else new_number = 0
   end
+  prime = true
   full_array = (2..new_number - 1).to_a
+   full_array.each do |i| 
+     if new_number % i == 0 
+       prime = false
+       return true
+  end
+end
 binding.pry
 end
 
