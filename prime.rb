6451f9f0139d7d1fccc6e5701152541469d2072d
Lucy Wall <lhwall@gmail.com>
2018-03-24 00:36:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,13 +8,20 @@ def prime?(integer)
   else new_number = 0
   end
   prime = true
+  if new_number == 2 
+    prime = true 
+  elsif new_number == 0 || new_number == 1 
+    prime = false 
+  else 
   full_array = (2..new_number - 1).to_a
    full_array.each do |i| 
      if new_number % i == 0 
        prime = false
-       return true
+       return false
   end
 end
+end
+return prime
 binding.pry
 end
 
