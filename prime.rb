351ed384d6b12407e31447a431f88631491be587
Lucy Wall <lhwall@gmail.com>
2018-03-24 00:30:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,12 @@
 require 'pry'
 # Add  code here!
 def prime?(integer)
-  
-  number_assign(integer)
-end
-
-def number_assign(integer)
- if integer >= 1
+    if integer >= 1
     new_number = integer
   elsif integer =< -1
     new_number = -1 * integer
   else new_number = 0
   end
+  full_array = [2..new_number - 1]
+binding.pry
 end
