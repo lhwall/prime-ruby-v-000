280d266c53af109d965e33ea0573771a80b88c10
Lucy Wall <lhwall@gmail.com>
2018-03-24 00:04:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,15 @@
 require 'pry'
 # Add  code here!
 def prime?(integer)
+  if integer < -1 
+    new_number = integer * -1
+  elsif integer > 1
+   new_number = integer
+  else new_number = 3
     is_prime = true
-  primes_array = [2..integer - 1]
+  primes_array = [2..new_number - 1]
   primes_array.each do |i|
-    if integer % i == 0
+    if new_number % i == 0
       return false
       is_prime = true
   end
